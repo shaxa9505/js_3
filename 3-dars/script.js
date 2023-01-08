2bcbafd8c4fdbc7b1568717c2ee90d9657056a12
shaxa_9505 <87860218+shaxa9505@users.noreply.github.com>
2023-01-08 21:03:40 +0500

    Add files via upload

---
+++ 3-dars/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+// Data Type = Number
+let number = 5.6; 
+console.log(8 / 0); // Infinity hech qande sonni 0 ga bo'lishi mumkin emas 
+console.log("Samar" * 8); // berda Nan chiqadi yani string bilan numberni bir biriga quwi bumedi
+
+
+// String
+const clientName = "Samar";
+
+
+// Boolean
+  // true | false
+const isMarried = false;
+const isCircleEarth = true
+
+// true = xa | to'g'ri
+// false = yoq | noto'g'ri
+
+
+// null yuq uzgaruvchini caqirsek chiqadi yani is not defined dp
+console.log(clientAge);
+
+
+// undefined bu bir uzgaruvchi yaratib unga qiymat yani value birmasek undefined ciqadi
+let und;
+console.log(und);
+
+
+
+// Object 
+const thief = {
+// key value
+  age: 30, // Number
+  jacket: "black", // String
+  isLong: false // Boolean
+}
+
+console.log(thief["isLong"]); // 1-yuli yani ancha eski yuli
+console.log(thief.isLong); // 2-yuli yangiro
+
+
+// Array || Massiv
+              //  0       1         2     3   4   5
+const colors = ["red", "black", "green", 10, [], {}];
+console.log(colors[0]);
+
+
+// BigInt
+const num = 238938372;
